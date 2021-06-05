d75a7fa4729026845e2b46e0b175721fefa4bf44
Birame SENE <bsene@pm.me>
2021-06-05 10:36:32 +0200

    Fizzbuzz kata(Coding Dojo) > stage 2 completed

--- codingdojo/fizzbuzz.js
+++ codingdojo/fizzbuzz.js
@@ -1,10 +1,12 @@
-const isFizz = (n) => n % 3 === 0;
-const isBuzz = (n) => n % 5 === 0;
+const numberContainsDigit = (n, d) => n.toString().includes(d.toString());
+const isFizz = (n) => n % 3 === 0 || numberContainsDigit(n, 3);
+const isBuzz = (n) => n % 5 === 0 || numberContainsDigit(n, 5);
+const isFizzBuzz = (n) => isFizz(n) && isBuzz(n);
 
 function create_fizzbuzz() {
   return {
     number(n) {
-      if (isFizz(n) && isBuzz(n)) return "FizzBuzz";
+      if (isFizzBuzz(n)) return "FizzBuzz";
       if (isBuzz(n)) return "Buzz";
       if (isFizz(n)) return "Fizz";
       return n.toString();
--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -1,52 +1,82 @@
 import { expect } from "chai";
 import { create_fizzbuzz } from "../codingdojo/fizzbuzz";
 
-const fizzbuzz = create_fizzbuzz();
 describe("Fizzbuzz tests suite", () => {
-  describe("Normal numbers", () => {
-    it("should return 1 when 1", () => {
+  let fizzbuzz;
+  beforeEach(() => {
+    fizzbuzz = create_fizzbuzz();
+  });
+  describe("is normal number", () => {
+    it("should return 1 with 1", () => {
       expect(fizzbuzz.number(1)).to.equal("1");
     });
-    it("should return 2 when 2", () => {
+    it("should return 2 with 2", () => {
       expect(fizzbuzz.number(2)).to.equal("2");
     });
-    it("should return 4 when 4", () => {
+    it("should return 4 with 4", () => {
       expect(fizzbuzz.number(4)).to.equal("4");
     });
-    it("should return 17 when 17", () => {
+    it("should return 17 with 17", () => {
       expect(fizzbuzz.number(17)).to.equal("17");
     });
   });
-  describe("Fizz numbers", () => {
-    it("should return Fizz when 3", () => {
-      expect(fizzbuzz.number(3)).to.equal("Fizz");
+  describe("is Fizz", () => {
+    describe("when is divible by 3", () => {
+      it("should return Fizz with 3", () => {
+        expect(fizzbuzz.number(3)).to.equal("Fizz");
+      });
+      it("should return Fizz with 9", () => {
+        expect(fizzbuzz.number(9)).to.equal("Fizz");
+      });
+      it("should return Fizz with 12", () => {
+        expect(fizzbuzz.number(12)).to.equal("Fizz");
+      });
     });
-    it("should return Fizz when 9", () => {
-      expect(fizzbuzz.number(9)).to.equal("Fizz");
-    });
-    it("should return Fizz when 12", () => {
-      expect(fizzbuzz.number(12)).to.equal("Fizz");
+
+    describe("when contains 3 in it", () => {
+      it("should return Fizz with 43", () => {
+        expect(fizzbuzz.number(43)).to.equal("Fizz");
+      });
+      it("should return Fizz with 34", () => {
+        expect(fizzbuzz.number(34)).to.equal("Fizz");
+      });
+      it("should return Fizz with 13", () => {
+        expect(fizzbuzz.number(13)).to.equal("Fizz");
+      });
     });
   });
-  describe("Buzz numbers", () => {
-    it("should return Buzz when 5", () => {
-      expect(fizzbuzz.number(5)).to.equal("Buzz");
+  describe("is Buzz", () => {
+    describe("when is divisible by 5", () => {
+      it("should return Buzz with 5", () => {
+        expect(fizzbuzz.number(5)).to.equal("Buzz");
+      });
+      it("should return Buzz with 10", () => {
+        expect(fizzbuzz.number(10)).to.equal("Buzz");
+      });
+      it("should return Buzz with 20", () => {
+        expect(fizzbuzz.number(20)).to.equal("Buzz");
+      });
     });
-    it("should return Buzz when 10", () => {
-      expect(fizzbuzz.number(10)).to.equal("Buzz");
-    });
-    it("should return Buzz when 20", () => {
-      expect(fizzbuzz.number(20)).to.equal("Buzz");
+    describe("when contains 5 in it", () => {
+      it("should return Buzz with 52", () => {
+        expect(fizzbuzz.number(52)).to.equal("Buzz");
+      });
+      it("should return Buzz with 56", () => {
+        expect(fizzbuzz.number(56)).to.equal("Buzz");
+      });
+      it("should return Buzz with 58", () => {
+        expect(fizzbuzz.number(58)).to.equal("Buzz");
+      });
     });
   });
   describe("FizzBuzz numbers", () => {
-    it("should return FizzBuzz when 15", () => {
+    it("should return FizzBuzz with 15", () => {
       expect(fizzbuzz.number(15)).to.equal("FizzBuzz");
     });
-    it("should return FizzBuzz when 30", () => {
+    it("should return FizzBuzz with 30", () => {
       expect(fizzbuzz.number(30)).to.equal("FizzBuzz");
     });
-    it("should return FizzBuzz when 45", () => {
+    it("should return FizzBuzz with 45", () => {
       expect(fizzbuzz.number(45)).to.equal("FizzBuzz");
     });
   });
