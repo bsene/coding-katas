ab8d392779dccaec46014523fb4427f8485b9e77
Birame S <birame.sene@keyprod.com>
2021-07-13 01:51:35 +0200

    Fizzbuzz kata: stage 1 completed

---
+++ src/Fizzbuzz.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App;
+
+
+class Fizzbuzz
+{
+    public function number(int $num): string
+    {
+        if ($this->isDivibleBy3($num) && $this->isDivisibleBy5($num)) return "FizzBuzz";
+
+        if ($this->isDivibleBy3($num)) return "Fizz";
+        if ($this->isDivisibleBy5($num)) return "Buzz";
+        return $num;
+    }
+
+    private function isDivibleBy3(int $num): bool
+    {
+        return $num % 3 === 0;
+    }
+
+    private function isDivisibleBy5(int $num): bool
+    {
+        return $num % 5 === 0;
+    }
+}
--- tests/FizzbuzzTest.php
+++ tests/FizzbuzzTest.php
@@ -1,9 +1,165 @@
 <?php
 
 
+use App\Fizzbuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzbuzzTest extends TestCase
 {
+    public function testShouldReturn1When1()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(1);
+
+        // Assert
+        $this->assertSame("1", $actual);
+    }
+
+    public function testShouldReturn2When2()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(2);
+
+        // Assert
+        $this->assertSame("2", $actual);
+    }
+
+    public function testShouldReturn4When4()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(4);
+
+        // Assert
+        $this->assertSame("4", $actual);
+    }
+
+    public function testShouldReturn7When7()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(7);
+
+        // Assert
+        $this->assertSame("7", $actual);
+    }
+
+    public function testShouldReturnFizzWhen3()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(3);
+
+        // Assert
+        $this->assertSame("Fizz", $actual);
+    }
+
+    public function testShouldReturnFizzWhen6()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(6);
+
+        // Assert
+        $this->assertSame("Fizz", $actual);
+    }
+
+    public function testShouldReturnFizzWhen9()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(9);
+
+        // Assert
+        $this->assertSame("Fizz", $actual);
+    }
+
+    public function testShouldReturnBuzzWhen5()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(5);
+
+        // Assert
+        $this->assertSame("Buzz", $actual);
+    }
+
+    public function testShouldReturnBuzzWhen10()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(10);
+
+        // Assert
+        $this->assertSame("Buzz", $actual);
+    }
+
+    public function testShouldReturnBuzzWhen20()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(20);
+
+        // Assert
+        $this->assertSame("Buzz", $actual);
+    }
+
+    public function testShouldReturnFizzBuzzWhen15()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(15);
+
+        // Assert
+        $this->assertSame("FizzBuzz", $actual);
+    }
+
+    public function testShouldReturnFizzBuzzWhen30()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(30);
+
+        // Assert
+        $this->assertSame("FizzBuzz", $actual);
+    }
+
+    public function testShouldReturnFizzBuzzWhen45()
+    {
+        // Arrange
+        $sut = new Fizzbuzz;
+
+        // Act
+        $actual = $sut->number(45);
+
+        // Assert
+        $this->assertSame("FizzBuzz", $actual);
+    }
 
 }
